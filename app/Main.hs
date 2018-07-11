621c658e0ae1dda09222093e9dd2926e4921c36a
andre <andre.videla@gmail.com>
2018-07-11 23:59:37 +0200

    add projection function for SI units

--- app/Main.hs
+++ app/Main.hs
@@ -11,6 +11,7 @@ filename = "LIV-222741+1.xls"
 newtype M_Volt = M_Volt Double
 newtype Volt = Volt Double
 newtype M_Apcm2 = M_Apcm2 Double
+newtype Apcm2 = Apcm2 Double
 newtype Ampere = Ampere Double
 newtype M_Ampere = M_Ampere Double
 newtype Wpm2 = Wpm2 Double
@@ -21,6 +22,9 @@ fromMiliAmp (M_Ampere value) = Ampere $ value / 1000
 
 fromMiliVolt :: M_Volt -> Volt
 fromMiliVolt (M_Volt value) = Volt $ value / 1000
+
+fromMiliAmpPerCm2 :: M_Apcm2 -> Apcm2
+fromMiliAmpPerCm2 (M_Apcm2 v) = Apcm2 $ v / 1000
 
 data SheetHeader = SheetHeader
     { name :: String
@@ -36,9 +40,31 @@ data SheetHeader = SheetHeader
     , isc :: M_Ampere
     }
 
+-- SI Projections for SheetHeader
+vocSI ::SheetHeader -> Volt
+vocSI = fromMiliVolt . voc
+
+jscSI :: SheetHeader -> Apcm2
+jscSI = fromMiliAmpPerCm2 . jsc
+
+vmppSI :: SheetHeader -> Volt
+vmppSI = fromMiliVolt . vmpp
+
+jmppSI :: SheetHeader -> Apcm2
+jmppSI = fromMiliAmpPerCm2 . jmpp
+
+iscSI :: SheetHeader -> Ampere
+iscSI = fromMiliAmp . isc
+
 getFile :: IO Xlsx
 getFile = do bs <- L.readFile filename
              return $ toXlsx bs
+
+getHeaders :: Xlsx -> [SheetHeader]
+getHeaders doc = map (getHeaderInfo . snd) (_xlSheets doc)
+
+getHeaderInfo :: Worksheet -> SheetHeader
+getHeaderInfo sheet = _
 
 main :: IO ()
 main = do file <- getFile
